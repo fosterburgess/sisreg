c6e90f2fefb57948c573bb36f34eec948c417d81
Michael Kimsal <michael@kimsal.com>
2023-02-19 17:26:51 -0500

    feat: teacher has metadata

--- app/Models/Reg/Teacher.php
+++ app/Models/Reg/Teacher.php
@@ -3,6 +3,7 @@
 namespace App\Models\Reg;
 
 use App\Models\User;
+use App\Traits\HasMetadata;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -11,6 +12,9 @@ class Teacher extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    use HasMetadata;
+
+    protected $guarded = ['id'];
 
     public function user()
     {
