92b480875b1c5f170fa816b21eb45bfc2db03f93
Michael Kimsal <michael@kimsal.com>
2023-02-19 17:26:45 -0500

    feat: add basic validation rules on teacher

--- app/Http/Requests/UpdateTeacherRequest.php
+++ app/Http/Requests/UpdateTeacherRequest.php
@@ -22,7 +22,21 @@ class UpdateTeacherRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'first_name' => ['string', 'nullable'],
+            'middle_name' => ['string', 'nullable'],
+            'last_name' => ['string', 'nullable'],
+            'preferred_name' => ['string', 'nullable'],
+            'full_name' => ['string', 'nullable'],
+            'pronouns' => ['string', 'nullable'],
+            'birthdate' => ['date', 'nullable'],
+            'phone' => ['string', 'nullable'],
+            'email' => ['email', 'nullable'],
+            'prefix' => ['string', 'nullable'],
+            'suffix' => ['string', 'nullable'],
+            'user_id' => ['integer', 'nullable'],
+            'metadata' => ['string', 'nullable'],
+            'external_id' => ['nullable'],
+            'vendor_id' => ['nullable'],
         ];
     }
 }
