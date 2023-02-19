57f9685bf20a975393a12d5534b9f135e9e4d9d4
Michael Kimsal <michael@kimsal.com>
2023-02-19 17:27:42 -0500

    feat: initial teacher service

--- app/Services/TeacherService.php
+++ app/Services/TeacherService.php
@@ -1,7 +1,16 @@
 <?php
 namespace App\Services;
 
+use App\Models\Reg\Teacher;
+
 class TeacherService
 {
 
+    public function createTeacher(array $validated): Teacher
+    {
+        $t = new Teacher();
+        $t->fill($validated);
+        $t->save();
+        return $t;
+    }
 }
