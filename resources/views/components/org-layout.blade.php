82fc5b1a8f4b1b2a55b73d6528183d96bba2fe40
Michael Kimsal <michael@kimsal.com>
2023-03-03 08:58:22 -0500

    chore: spacing/layout experiment

--- resources/views/components/org-layout.blade.php
+++ resources/views/components/org-layout.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12 ">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="px-4 bg-white overflow shadow-xl sm:rounded-lg">
+            <div class="px-4 py-4 bg-white overflow shadow-xl sm:rounded-lg">
                 {{$slot}}
             </div>
         </div>
--- resources/views/org/index.blade.php
+++ resources/views/org/index.blade.php
@@ -1,11 +1,13 @@
 <x-org-layout>
     @if($canCreateTopLevel)
+        <div class="mb-4">
     <a
-            class="bg-blue-100 px-4 py-4 rounded ml-4 my-4"
+            class="mt-4 bg-blue-100 px-4 py-2 rounded ml-4 my-4 mb-4"
                          color="red"
                          href="{{route('org.create')}}">
         add an organization
         </a>
+        </div>
     @endif
 
 
